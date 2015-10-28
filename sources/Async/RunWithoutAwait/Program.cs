8e8b6edad9f8de0998ba5842df0c6c4bd4a7da4c
dmitriy.litichevskiy <dmitry.litichevskiy@byndyusoft.com>
2015-10-29 00:06:16 +0500

    Example for ecxeptions handling for task without waiting results

---
+++ sources/Async/RunWithoutAwait/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace RunWithoutAwait
+{
+    using System;
+    using System.Net;
+    using System.Threading.Tasks;
+
+    internal static class Program
+    {
+        private static async Task DownloadPageAsync(string uri)
+        {
+            using (var webclient = new WebClient())
+            {
+                var content = await webclient.DownloadStringTaskAsync(uri);
+                Console.WriteLine(content.Length);
+                throw new InvalidOperationException("Something go wrong");
+            }
+        }
+
+        private static void Main()
+        {
+            var task = DownloadPageAsync("http://www.microsoft.com/");
+            task.ContinueWith(t => t.Exception.Handle(e =>
+                                                      {
+                                                          Console.WriteLine(e.Message);
+                                                          return true;
+                                                      }),
+                TaskContinuationOptions.OnlyOnFaulted);
+            Console.WriteLine("Something happening");
+            Console.ReadKey();
+        }
+    }
+}
