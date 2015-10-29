6f19279e55747148a710c1d26a553ef604cf785f
dmitriy.litichevskiy <dmitry.litichevskiy@byndyusoft.com>
2015-10-29 23:56:30 +0500

    Example for task cancellation

---
+++ sources/Async/TaskCancellation/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace TaskCancellation
+{
+    using System;
+    using System.Net.Http;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    static class Program
+    {
+        private static async Task DownloadPageAsync(string uri, CancellationToken token)
+        {
+            using (var client = new HttpClient())
+            using (var response = await client.GetAsync(uri, token))
+            {
+                var content = await response.Content.ReadAsStringAsync();
+                Console.WriteLine(content.Length);
+            }
+        }
+
+        static void Main()
+        {
+            using (var cts = new CancellationTokenSource())
+            {
+                cts.CancelAfter(TimeSpan.FromMilliseconds(100));
+                try
+                {
+                    var task = DownloadPageAsync("http://www.microsoft.com/", cts.Token);
+                    task.Wait(cts.Token);
+                }
+                catch (OperationCanceledException)
+                {
+                    Console.WriteLine("Wait cancelled");
+                }
+                catch (AggregateException)
+                {
+                    Console.WriteLine("Downloading cancelled");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
