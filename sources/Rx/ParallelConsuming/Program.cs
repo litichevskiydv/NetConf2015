11bdb086336144e9c23fe1606029f0a71af9a5c8
dmitriy.litichevskiy <dmitry.litichevskiy@byndyusoft.com>
2016-01-06 22:55:49 +0500

    Rx, consuming items in generation threads

---
+++ sources/Rx/ParallelConsuming/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace ParallelConsuming
+{
+    using System;
+    using System.Globalization;
+    using System.Reactive.Subjects;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    internal static class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Main ManagedThreadId:{0}", Thread.CurrentThread.ManagedThreadId);
+
+            var queue = new Subject<string>();
+            queue
+                .Subscribe(x =>
+                           {
+                               Console.WriteLine("[{0}] Consuming: {1}", Thread.CurrentThread.ManagedThreadId, x);
+                           });
+
+            // Consuming items in generation threads
+            var task1 = Task.Run(() =>
+                                 {
+                                     Console.WriteLine("Producer ManagedThreadId:{0}", Thread.CurrentThread.ManagedThreadId);
+                                     for (var i = 0; i < 100; i++)
+                                         queue.OnNext((2*i).ToString(CultureInfo.InvariantCulture));
+                                 });
+            var task2 = Task.Run(() =>
+                                 {
+                                     Console.WriteLine("Producer ManagedThreadId:{0}", Thread.CurrentThread.ManagedThreadId);
+                                     for (var i = 0; i < 100; i++)
+                                         queue.OnNext((2*i + 1).ToString(CultureInfo.InvariantCulture));
+                                 });
+            Task.WaitAll(task1, task2);
+
+            Console.ReadKey();
+        }
+    }
+}
